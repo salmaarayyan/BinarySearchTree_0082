b05de4f99118b2999e6bc24b04e4a227520a1665
salmaarayyan <salmaarayyan@gmail.com>
2024-06-14 13:59:52 +0700

    mendeklarasikan fungsi search dengan parameter element, parent, dan currentNode yang berfungsi mencari Node yang akan menjadi parent dari sebuah node yang akan dimasukkan

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -30,6 +30,7 @@ public:
         newNode->rightchild = NULL;  //make the right child of the new node point to NULL
 
         Node* parent = NULL;
-        Node* currentNode = NULL; 
+        Node* currentNode = NULL;
+        search(element, parent, currentNode);  //locate the node which will be the parent of the node to be inserted
     }
 };

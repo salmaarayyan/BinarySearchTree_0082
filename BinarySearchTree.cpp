c4b6a6675d8ed5bd274b02f75b999fc7543e35f0
salmaarayyan <salmaarayyan@gmail.com>
2024-06-14 13:55:26 +0700

    mengalokasikan memori untuk node baru dan memberi nilai pada info yaitu element

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -24,6 +24,9 @@ public:
         ROOT = NULL;  //initializing ROOT to NULL
     }
     void insert(string element) { //insert a node in the binary search tree
-
+        Node* newNode = new Node(element, NULL, NULL);  //allocate memory for the new node
+        newNode->info = element;  //assign value to the data field of the new node
+        newNode->leftchild = NULL;  //make the left child of the new node point to NULL
+        newNode->rightchild = NULL;  //make the right child of the new node point to NULL 
     }
 };

37f8a156c07793cd09f3665afa42aeb62cc745e7
salmaarayyan <salmaarayyan@gmail.com>
2024-06-14 14:10:30 +0700

    membuat fungsi search dengan parameter element, parent, dan currentNode yang berfungsi untuk mencari Node

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -43,4 +43,17 @@ public:
             parent->rightchild = newNode;  //make the right child of the parent point to the new node
         }
     }
+
+    void search(string element, Node*& parent, Node*& currentNode) {
+        //this function searches the currentNode of the specified Node as well as the current Node of its parent
+        currentNode = ROOT;
+        parent = NULL;
+        while ((currentNode != NULL) && (currentNode->info != element)) {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }
+    }
 };

0367f109914771ad6108c4272be5baa7875a35d3
salmaarayyan <salmaarayyan@gmail.com>
2024-06-14 14:17:45 +0700

    membuat fungsi postorder untuk menampilkan node secara postorder yang dimulai dari leftchild kemudian rightchild dan terakhir root

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -80,4 +80,17 @@ public:
             preorder(ptr->rightchild);
         }
     }
+
+    void postorder(Node* ptr) {
+        //performs the postorder traversal of the tree
+        if (ROOT == NULL) {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL) {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
 };
